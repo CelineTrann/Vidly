92de411c825197315b86f80e0083f1b84fe5624e
Celine Tran <celinephoungtran@gmail.com>
2022-05-26 10:15:26 -0400

    Adding Validation to Customer Name field

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -37,14 +37,14 @@ namespace Vidly.Controllers
         [HttpPost]
         public ActionResult Save(Customer customer)
         {
-            /*if(!ModelState.IsValid) {
-                var viewMode = new CustomerFormViewModel
+            if(!ModelState.IsValid) {
+                var viewModel = new CustomerFormViewModel
                 {
                     Customer = customer,
                     MembershipTypes = _context.MembershipTypes.ToList()
                 };
-                return View("CustomerForm");
-            }*/
+                return View("CustomerForm", viewModel);
+            }
 
             if (customer.Id == 0)
                 _context.Customers.Add(customer);
