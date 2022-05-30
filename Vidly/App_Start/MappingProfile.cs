53954d740fe3f3ff7e73a1a4c30ca5d113d05ab6
CelineTran <celine.tran@alcumus.com>
2022-05-30 16:17:01 -0400

    Returning Hierarchal Data. Allow for membership name to be called from api and therefore displayed on the customer page

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -15,10 +15,15 @@ namespace Vidly.App_Start
             // API -> Outbound
             Mapper.CreateMap<Customer, CustomerDto>();
             // API <- Inbound
-            Mapper.CreateMap<CustomerDto, Customer>().ForMember(c => c.Id, opt => opt.Ignore());
+            Mapper.CreateMap<CustomerDto, Customer>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+
+            Mapper.CreateMap<MembershipType, MembershipTypeDto>();
+            Mapper.CreateMap<MembershipTypeDto, MembershipType>();
 
             Mapper.CreateMap<Movie, MovieDto>();
-            Mapper.CreateMap<MovieDto, Movie>().ForMember(m => m.Id, opt => opt.Ignore());
+            Mapper.CreateMap<MovieDto, Movie>()
+                .ForMember(m => m.Id, opt => opt.Ignore());
         }
     }
 }
