93eff843eaa5e354e37577484e10b275bef47a24
CelineTran <celine.tran@alcumus.com>
2022-06-01 11:40:18 -0400

    10. Create template for action required for problem

---
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Models;
+using Vidly.Dtos;
+
+namespace Vidly.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        public ApplicationDbContext _context;
+
+        public NewRentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // POST api/newRental
+        [HttpPost]
+        public IHttpActionResult CreateNewRental (NewRentalDto newRental)
+        {
+            return Ok();
+        }
+    }
+}
